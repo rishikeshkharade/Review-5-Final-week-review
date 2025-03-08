2f21f0fa1979c20d887016d8b250e1b8c98d15bc
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-03-08 12:59:39 +0530

    Main of All Programs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Review_5__Final_week_Review_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //PrimeNumberChecker.PrimeChecker();
+        //SortingNames.SortingNamesMain();
+        StackImplementation.StackOperations();
+        //AgeException.CheckAge();
+
+
+    }
+}
